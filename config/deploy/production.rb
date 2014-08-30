334a8152c1a520983882bf94c40d6ab3d8509d39
Bartosz Nowak <sztosz@gmail.com>
2014-08-30 19:14:37 +0200

    Capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@ role :db,  %w{root@iza.sztosz.tk}
 # something that quacks like a hash can be used to set
 # extended properties on the server.
 set :password, ask('Server password', nil)
-server 'iza.sztosz.tk', user: 'root', port: 22, password: fetch(:password), roles: %w{web app db}
+server 'iza.sztosz.tk', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
