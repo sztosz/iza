da0919b4ff837fc234e055d624a3bb423b20ec01
Bartosz Nowak <sztosz@gmail.com>
2014-09-13 20:38:11 +0200

    smtp for production fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      # domain:               'example.com',
+      user_name:            Figaro.env.smtp_user_name!,
+      password:             Figaro.env.smtp_user_password!,
+      authentication:       :plain,
+      enable_starttls_auto: true,
+      openssl_verify_mode:  'none'
+  }
 end
